48dc66a3800be6e6be27f59da0735c44c914d739
zhangjianxun <dizzy21c@gmail.com>
2020-01-21 20:31:39 +0800

    code backup

---
+++ QAStrategy/Stock_Example2.py
@@ -0,0 +1,26 @@
+import QUANTAXIS as QA
+# from QAStrategy.qastockbase_ext import QAStrategyStockBaseExt
+from qastockbase_ext import QAStrategyStockBaseExt
+
+
+class strategy2(QAStrategyStockBaseExt):
+    def on_bar(self, data):
+        # print(data)
+        # print(self.get_positions('000001'))
+        # print(self.market_data)
+        
+        code = data.name[1]
+        # print('---------------under is 当前全市场的market_data --------------')
+        
+        # print(self.get_current_marketdata())
+        # print('---------------under is 当前品种的market_data --------------')
+        # print(self.get_code_marketdata(code))
+        # print("step...")
+        print(code)
+        #print(self.running_time)
+        # input()
+
+if __name__ == '__main__':
+    s = strategy2(code=['000001', '000002'], frequence='day', start='2019-01-01', end='2019-02-01', strategy_id='x')
+    s.run_sim()
+#     s.run_backtest()
